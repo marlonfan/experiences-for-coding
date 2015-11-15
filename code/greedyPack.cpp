87bea684c7c6b5267b33b56ccddad7e754708683
MarlonFan <marlon@tfan.net>
2015-11-15 23:53:29 +0800

    提提~

--- code/greedyPack.cpp
+++ code/greedyPack.cpp
@@ -1,17 +1,40 @@
 #include <stdio.h>
 
-struct product {
-	char name[10];
+typedef struct tagObject {
 	int weight;
 	int price;
-};
+	int status; //0: 未选中 1: 已选中 2: 已经不可选
+} OBJECT;
+
+typedef struct tagKnapsackProblem {
+	std::vector<OBJECT> objs;
+	int totalC;
+} KNAPSACK_PROBLEM;
+
+void GreedyAlgo(KNAPSACK_PROBLEM *problem, SELECT_POLICY spFunc) {
+	int idx;
+	int ntc = 0;
+	
+	while((idx = spFunc(problem->objs, problem->totalC - ntc)) != 1)
+	{
+		if ((ntc + problem->objs[idx].weight) <= problem->totalC) {
+			ntc += problem->objs[idx].weight;
+		} else {
+			problem->obks[idx].status = 2;
+		}
+	}
+	
+	PrintResult(problem->objs);
+}
+
+int Choosefunc1(std::vector<OBJECT>& objs, int c) {
+	int index = -1;
+	int mp = 0;
+	for (int i = 0; i < static_cast<int>(objs.size()); i++) {
+		
+	}
+}
 
 int main() {
-	struct product productsList[2];
 	
-	productsList[0] = {'marlon', 135, 10000000};
-	
-	printf("物品名字: %c, 物品重量: %d, 物品价格: %d", productsList[0].name, productsList[0].weight, productsList[0].price);
-	
-	return 0;
 }
